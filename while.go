6e6dd329e4a565e7d875861dfc308511d59b77ad
acke <knut.funkel@gmail.com>
2023-07-18 13:25:12 +0200

    While loop that runs until you guess 56

---
+++ while.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+)
+
+func ask() (int) {
+  var input int
+  fmt.Print("I am thinking of a number between 1-100: ")
+  // Get the input from the user
+  fmt.Scan(&input)
+  return input
+}
+
+func main() {
+  var guess int
+
+  // WRITE THE INDEFINITE LOOP CONTAINING THE ASK FUNCTION BELOW
+
+  for guess != 56 {
+    guess = ask()
+  }
+  
+  if guess == 56 {
+    fmt.Println("You are correct! You may go through to the treasure!")
+  }
+}
+
