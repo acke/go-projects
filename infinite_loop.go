5b990622bf729c532bb28edba9b7933e8d91f1a5
acke <knut.funkel@gmail.com>
2023-07-18 13:29:59 +0200

    Infinite loop adding guests to a gym

---
+++ infinite_loop.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+)
+
+var number int
+
+func count() {
+  var input int
+  fmt.Print("Number of guests: ")
+  fmt.Scan(&input)
+  number = number + input
+  fmt.Println("Total guests:", number)
+}
+
+func main() {
+
+  // INFINITE LOOP CONTAINING THE COUNT FUNCTION BELOW
+  for {
+    count()
+  }
+}
+
