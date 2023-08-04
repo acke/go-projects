66eab2308325ecae9d15c32574ba69192d425862
acke <knut.funkel@gmail.com>
2023-08-04 20:47:30 +0200

    Added a loop that uses break and continue to manipulate the loop execution.

---
+++ count.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+
+  for count := 0; count < 20; count++ {
+    if count == 8 {
+      continue
+    }
+
+    if count == 15 {
+      break
+    }
+    
+    fmt.Println(count)
+  }
+
+}
