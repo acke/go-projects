df0bba19c0d3a362d51f2e2dd353defee59737ff
acke <knut.funkel@gmail.com>
2023-08-07 15:31:07 +0200

    Printed length and capacity of a slice

---
+++ lengthAndCapacityOfSlices.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    myTutors := []string{"Kirsty", "Mishell", "Jose", "Neil"}
+    fmt.Println(myTutors)
+    fmt.Println(len(myTutors))
+    fmt.Println(cap(myTutors))
+}
