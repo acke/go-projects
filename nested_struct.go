92c3d5e2ddd40a00a0bd35ec996a349be9a2ad15
acke <knut.funkel@gmail.com>
2023-08-09 14:40:00 +0200

    Accessing anonymous values in nested structs

---
+++ nested_struct.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	x int
+	y int
+}
+
+type Circle struct {
+  Point
+  radius int
+}
+
+func main() {
+	circle := Circle{Point{4,5}, 2}
+	
+  fmt.Println(circle.x)
+	
+  fmt.Println(circle)
+}
+
