6a04af7366c1cc7d52bafe2060a02e364268e4ab
acke <knut.funkel@gmail.com>
2023-08-10 17:23:42 +0200

    Used Sprintf() to format the string to print.

---
+++ Sprintf.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  template := "I wish I had a %v."
+  pet := "puppy"
+  
+  var wish string
+  
+  wish = fmt.Sprintf(template, pet) 
+  
+  fmt.Println(wish)
+}
+
---
+++ Sprintln.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  step1 := "Breathe in..."
+  step2 := "Breathe out..."
+  
+  meditation := fmt.Sprintln(step1, step2)
+  fmt.Print(meditation)
+}
+
