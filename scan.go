f4ad6466f3cda6240a7879b055bf3ffa6464c404
acke <knut.funkel@gmail.com>
2023-08-10 17:26:45 +0200

    Use fmt.Scan() to capture input from the terminal

---
+++ scan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("What would you like for lunch?")
+  
+  var food string
+  
+  fmt.Scan(&food)
+
+  fmt.Printf("Sure, we can have %v for lunch.", food)
+}
+
