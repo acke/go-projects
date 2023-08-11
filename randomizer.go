031ced33d27c6eda2e9905081ccf900eadef559b
acke <knut.funkel@gmail.com>
2023-08-12 00:23:18 +0200

    Using rand.Intn() from math/rand to generate a random number.

---
+++ randomizer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+  "math/rand"
+)
+
+func main() {
+	
+  amountLeft := rand.Intn(10000)
+  
+  fmt.Println("amountLeft is: ", amountLeft)
+  
+	if amountLeft > 5000 {
+		fmt.Println("What should I spend this on?")
+  } else {
+    fmt.Println("Where did all my money go?")
+  }
+}
+
