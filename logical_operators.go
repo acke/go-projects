4e7f632d5b488070b20eaddbc9db45149494a1f7
acke <knut.funkel@gmail.com>
2023-08-11 20:00:24 +0200

    Using logical operators

---
+++ logical_operators.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	rightTime := true
+	rightPlace := true
+
+	if rightTime && rightPlace {
+		fmt.Println("We're outta here!")
+	} else {
+		fmt.Println("Be patient...")
+	}
+
+
+	enoughRobbers := false
+	enoughBags := true
+
+	if enoughRobbers || enoughBags {
+		fmt.Println("Grab everything!")
+	} else {
+		fmt.Println("Grab whatever you can!")
+	}
+}
+
