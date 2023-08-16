9c4a7f1d113419b28bbf83b9412ea28e55a7895e
acke <knut.funkel@gmail.com>
2023-08-16 13:59:18 +0200

    Removing unnecessary comments

--- while.go
+++ while.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
-func ask() (int) {
-  var input int
-  fmt.Print("I am thinking of a number between 1-100: ")
-  // Get the input from the user
-  fmt.Scan(&input)
-  return input
+func ask() int {
+	var input int
+	fmt.Print("I am thinking of a number between 1-100: ")
+	// Get the input from the user
+	fmt.Scan(&input)
+	return input
 }
 
 func main() {
-  var guess int
+	var guess int
 
-  // WRITE THE INDEFINITE LOOP CONTAINING THE ASK FUNCTION BELOW
+	for guess != 56 {
+		guess = ask()
+	}
 
-  for guess != 56 {
-    guess = ask()
-  }
-  
-  if guess == 56 {
-    fmt.Println("You are correct! You may go through to the treasure!")
-  }
+	if guess == 56 {
+		fmt.Println("You are correct! You may go through to the treasure!")
+	}
 }
-
