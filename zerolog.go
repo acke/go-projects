14c9115b5ac380648814f24eae8950db5d667697
acke <knut.funkel@gmail.com>
2023-08-21 14:08:40 +0200

    Added dependencies and started to use the zerolog

---
+++ zerolog.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Print("hello world")
+}
