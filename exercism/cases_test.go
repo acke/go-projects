26b6300adee74bd755881b6b5c2ab52c6e87c82b
acke <knut.funkel@gmail.com>
2023-11-15 17:24:32 +0100

    Implemented the Hamming exercise with a for loop.

---
+++ exercism/cases_test.go
@@ -0,0 +1,57 @@
+package hamming
+
+// This is an auto-generated file. Do not change it manually. Run the generator to update the file.
+// See https://github.com/exercism/go#synchronizing-tests-and-instructions
+// Source: exercism/problem-specifications
+// Commit: 7a8722a Reorder keys (#1960)
+
+var testCases = []struct {
+	description string
+	s1          string
+	s2          string
+	want        int
+	expectError bool
+}{
+	{
+		description: "empty strands",
+		s1:          "",
+		s2:          "",
+		want:        0,
+		expectError: false,
+	},
+	{
+		description: "single letter identical strands",
+		s1:          "A",
+		s2:          "A",
+		want:        0,
+		expectError: false,
+	},
+	{
+		description: "single letter different strands",
+		s1:          "G",
+		s2:          "T",
+		want:        1,
+		expectError: false,
+	},
+	{
+		description: "long identical strands",
+		s1:          "GGACTGAAATCTG",
+		s2:          "GGACTGAAATCTG",
+		want:        0,
+		expectError: false,
+	},
+	{
+		description: "long different strands",
+		s1:          "GGACGGATTCTG",
+		s2:          "AGGACGGATTCT",
+		want:        9,
+		expectError: false,
+	},
+	{
+		description: "disallow first strand longer",
+		s1:          "AATG",
+		s2:          "AAA",
+		want:        0,
+		expectError: true,
+	},
+	{
---
+++ exercism/hamming.go
@@ -0,0 +1,16 @@
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (hammingDistance int, err error) {
+    if len(a) != len(b) { return 0, errors.New("Sizes not equal") }
+
+    for i := range a {
+        if (a[i] != b[i]){
+            hammingDistance++
+        }
+    }
+
+    return
+}
+
