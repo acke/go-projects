899ddcf069ca1f0775ecfbc8b2f9b40a652e1650
acke <knut.funkel@snyk.io>
2024-08-24 09:39:24 +0200

    Add support for multiple ppls

--- go-tutorials/greetings/greetings.go
+++ go-tutorials/greetings/greetings.go
@@ -17,6 +17,25 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random.
 func randomFormat() string {
@@ -27,7 +46,6 @@ func randomFormat() string {
 		"Hail, %v! Well met!",
 	}
 
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
+	// Return one of the message formats selected at random.
 	return formats[rand.Intn(len(formats))]
 }
